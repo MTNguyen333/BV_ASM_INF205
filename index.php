ea54f07665bd6d726e5b6c375e5ed6b84e547468
MTNguyen333 <minhthu3394@gmail.com>
2015-08-20 13:52:58 +0700

    BV_ASM
    Bluemix PHP Project

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP Starter Application</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<link rel="stylesheet" href="style.css" />
+</head>
+<body>
+	<table>
+		<tr>
+			<td style='width: 30%;'><img class = 'newappIcon' src='images/newapp-icon.png'>
+			</td>
+			<td>
+				<h1 id = "message"><?php echo "BAO VE ASSIGNMENT INF 205 - PT9307 - PS02094"; ?>
+</h1>
+			</td>
+		</tr>
+	</table>
+</body>
+</html>
